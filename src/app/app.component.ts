9407dc4e0ae7653b679d36272748866c77eb985a
tawfeq-at-706820225002 <mahmmedtawfeqamirie@gmail.com>
2021-12-28 00:11:46 +0200

    upload R lover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'love';
+
+  constructor(){
+      // let audio = new Audio();
+      // audio.src = "assets/audio/Habibi_Yeah.mp3";
+      // audio.load();
+      // audio.play();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BeatComponent } from './pages/beat/beat.component';
+import { MessageComponent } from './pages/message/message.component';
+import { NavComponent } from './pages/nav/nav.component';
+import { ReasonComponent } from './pages/reason/reason.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatExpansionModule} from '@angular/material/expansion';
+import { AboutComponent } from './pages/about/about.component';
+import { RComponent } from './pages/r/r.component';
+import { StarComponent } from './pages/star/star.component';
+import { CardComponent } from './pages/card/card.component';
+import { HeartBeatComponent } from './pages/heart-beat/heart-beat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BeatComponent,
+    MessageComponent,
+    NavComponent,
+    ReasonComponent,
+    AboutComponent,
+    RComponent,
+    StarComponent,
+    CardComponent,
+    HeartBeatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
